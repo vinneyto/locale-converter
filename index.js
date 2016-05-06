29e783d64e66e3327d500f7207be35f06a0226c2
Leonid Vinnichenko <vinneyto@gmail.com>
2016-05-06 17:19:35 +0300

    добавил утилиту для сравнения

--- index.js
+++ index.js
@@ -17,11 +17,20 @@ var argv = yargs
   .command('json2xls', 'convert multiple json locale files to single xlsx')
   .demand(3)
   .example('$0 json2xls input_dir test.xlsx', 'covert multiple json locale files from input dir to single test.xlsx file')
+  .command('compare', 'compare target.xls width reference.xls; all changes in target.xls will be display relative to reference.xls')
+  .demand(4)
+  .example('$0 compare reference.xls target.xls result.json', 'compare target.xls with reference.xls and save difference in result.json')
+  .string('l')
+  .alias('l', 'locales')
+  .describe('l', 'locales to compare')
+  .string('t')
+  .alias('t', 'types')
+  .describe('t', 'types of changes: a - added, r - removed, c - changed. -t arc')
   .argv;
 
 function main() {
   try {
-    var commands = ['xls2json', 'json2xls'];
+    var commands = ['xls2json', 'json2xls', 'compare'];
 
     if (commands.indexOf(argv._[0]) >= 0) {
       require('./lib/cmd/' + argv._[0])(argv);
